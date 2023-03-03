4f94ed085e89600677f8d3a962495f0f50752179
Khafra <42794878+KhafraDev@users.noreply.github.com>
2023-03-03 16:54:30 -0500

    fix: stop bothering with freebsd

--- index.js
+++ index.js
@@ -132,21 +132,6 @@ switch (platform) {
         throw new Error(`Unsupported architecture on macOS: ${arch}`)
     }
     break
-  case 'freebsd':
-    if (arch !== 'x64') {
-      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
-    }
-    localFileExisted = existsSync(join(__dirname, 'qrcode.freebsd-x64.node'))
-    try {
-      if (localFileExisted) {
-        nativeBinding = require('./qrcode.freebsd-x64.node')
-      } else {
-        nativeBinding = require('@khaf/qrcode-freebsd-x64')
-      }
-    } catch (e) {
-      loadError = e
-    }
-    break
   case 'linux':
     switch (arch) {
       case 'x64':
--- npm/freebsd-x64/README.md
+++
@@ -1,3 +0,0 @@
-# `@khaf/qrcode-freebsd-x64`
-
-This is the **x86_64-unknown-freebsd** binary for `@khaf/qrcode`
